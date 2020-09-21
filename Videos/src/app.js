03d578819f6e85aa11f4f6c868532b7215b9b77b
mrA121 <royshivank@gmail.com>
2020-09-22 03:51:10 +0530

    changes in the videos app file

--- Videos/src/app.js
+++ Videos/src/app.js
@@ -26,7 +26,7 @@ const fileFilter = (req, file, cb) => {
 app.use(bodyParser.json()); 
 
 app.use(
-    multer({storage:fileStorage,fileFilter:fileFilter}).single('video')
+    multer({storage:fileStorage,fileFilter:fileFilter}).single('data')
     )
 
 app.use("api/video",videoRoutes)
