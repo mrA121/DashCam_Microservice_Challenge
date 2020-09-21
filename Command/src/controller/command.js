baf65f6807e2bb45b2e393bf5d9765d7b44a02d8
mrA121 <royshivank@gmail.com>
2020-09-22 02:18:09 +0530

    Command Service

--- Command/src/controller/command.js
+++ Command/src/controller/command.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const {SECRET_KEY}=require('../config/index')
+
 exports.command=(req,res,next)=>{
     res.status(200).json({type: 'COMMAND_RESPONSE',response: 'OK/Failure'})
 }
